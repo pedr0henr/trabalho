ac61ef41a4aa860b513392e048f595a2b5cdd3be
pedro <andradep012@gmail.com>
2023-12-01 15:51:30 -0300

    trabalho

---
+++ banco.php
@@ -0,0 +1,17 @@
+<?php
+$servidor = "localhost";
+$base = "dbinb0243";
+$usuario = "inb0243user";
+$senha = "alunos";
+try {
+$conn = new PDO("mysql:host=$servidor;dbname=$base",$usuario,$senha);
+$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+echo "<center><h1>Conectado do Servidor Web com Sucesso a base: </h1>" .$base;
+echo "<br><h2><font color=red>9142</font></h2><br>";
+echo "<br><h2><font color=red>9080</font></h2></center>";
+
+} catch(PDOException $e) {
+echo "Falha na conexão:" .$e->getMessage();
+}
+?>
+
---
+++ teste.php
@@ -0,0 +1,6 @@
+<?php
+echo"<center><h1>Pagina gerada no servidor
+Srv9142-9080</h1></center>";
+echo"<center><h1>Seja bem-vindo a página local do servidor WEB:instalado por <br>  Pedro Henrique -9142 <br> </h1><center><br> <h1> Gustavo Rudolf 9080</h1><br>";
+phpinfo();
+?>
